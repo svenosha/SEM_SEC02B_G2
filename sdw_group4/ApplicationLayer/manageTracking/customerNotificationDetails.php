857c273f57e107e3960cfd83f7691858ff3b2e38
Huiyin13 <83337412+Huiyin13@users.noreply.github.com>
2021-05-23 16:11:11 +0800

    Update customerNotificationDetails.php

--- sdw_group4/ApplicationLayer/manageTracking/customerNotificationDetails.php
+++ sdw_group4/ApplicationLayer/manageTracking/customerNotificationDetails.php
@@ -3,14 +3,15 @@
 
     session_start();
 
+    $orderID = $_GET['orderID'];
     $notification = new manageTrackingController();
-    $data = $notification->viewCustomer();
+    $data = $notification->viewCustomerOrderDetails($orderID);
 
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Customer Notification</title>
+        <title>Customer Delivery Details</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="ExternalCSS/topnav.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -33,16 +34,16 @@
     </head>
     <body>
         <div class="topnav">
-            <a href="../../ApplicationLayer/manageOrder/customerHomePage.php?spID=<?=$_SESSION['spID']?>"><img src="Image/largerlogo.png" width="110px" height="70px"><label style="font-size: 120%; padding-right: 5px;">Homepage</label></a>
+            <a href="../../ApplicationLayer/manageOrder/customerHomePage.php?spID=<?=$_SESSION['custID']?>"><img src="Image/largerlogo.png" width="110px" height="70px"><label style="font-size: 120%; padding-right: 5px;">Homepage</label></a>
             
             <div class="topnav-right">
-                <a href="../../ApplicationLayer/manageUserProfile/customerProfile.php?spID=<?=$_SESSION['spID']?>"><i class="fa fa-user" aria-hidden="true" style="font-size: 50px; padding-right: 5px; padding-left: 5px; padding-top: 22%; padding-bottom: 22%;"></i></a>
+                <a href="../../ApplicationLayer/manageUserProfile/customerProfile.php?spID=<?=$_SESSION['custID']?>"><i class="fa fa-user" aria-hidden="true" style="font-size: 50px; padding-right: 5px; padding-left: 5px; padding-top: 22%; padding-bottom: 22%;"></i></a>
             </div>
         </div>
 
         <div class="logout"><a href="../manageLoginAndRegister/userLogin.php">Logout</a></div>
         <center>
-        <h3 style="margin-left: 1em; margin-top: 1em; text-decoration: underline;">Order Notification</h3>
+        <h3 style="margin-left: 1em; margin-top: 1em; text-decoration: underline;">Delivery Details</h3>
         <br><br>
 
             <div style="margin-left: 1.5em;">
@@ -50,31 +51,46 @@
                 <table>
                     <tr>
                         <td width="150"><center><b>Item Name</b></center></td>
-                        <td width="130"><center><b>Unit Price (RM)</b></center></td>
-                        <td width="130"><center><b>Quantity</b></center></td>
-                        <td width="200"><center><b>Status</b></center></td>
+                        <?php foreach($data as $row){ ?>
+                        <td><?=$row['itemname']?></td>
                         
                     </tr>
-                    <?php foreach($data as $row){ ?>
                     <tr>
-                        <td><?=$row['itemname']?></td>
-                        <td><?=$row['itemprice']?></td>
-                        <td><?=$row['itemquantity']?></td>
-                        <form action="" method="POST">
-                            <td style="text-align: center;">
+                        <td width="150"><center><b>Delivery Status</b></center></td>
+                        
+                            <td style="text-align: center;" >
                                 <?php 
-                                    if($row['status'] == 3){
+                                    if($row['status'] == 2){
+                                        echo "Order has been accepted by service provider";
+                                        echo "<br />";
+                                        echo "Order has been pickup by runner";
+                                    }
+                                    else if($row['status'] == 3){
+                                        echo "Order has been accepted by service provider";
+                                        echo "<br />";
+                                        echo "Order has been pickup by runner";
+                                        echo "<br />";
                                         echo "Delivery Successful! Enjoy!";
                                     }
+                                    else if($row['status'] == 4){
+                                        echo "Order has been rejected by service provider";
+                                        echo "<br />";
+                                        echo "Will be refund ASAP!";
+                                    }
                                     else if($row['status'] == 7){
+                                        echo "Order has been accepted by service provider";
+                                        echo "<br />";
+                                        echo "Order has been pickuped by runner";
+                                        echo "<br />";
                                         echo "Delivery Fail! Will be refund ASAP!";
+                                    }
+                                    else if($row['status'] == 1){
+                                        echo "Order pending accept by service provider.";
                                     }
                                 ?>
                             </td>
-
                         </form>
-                        
-                    <?php } ?>
+                        <?php } ?>
                     </tr>
                 </table>
             </div>
